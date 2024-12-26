b276f1ba0ea2f4340d34d5df590f6e487ff1ee3b
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2024-12-26 20:41:16 +0100

    model non necessaire

--- models/attachment.js
+++ models/attachment.js
@@ -1,27 +1,28 @@
-const mongoose = require('mongoose')
+// this file is not used : Mariem remarque
+// const mongoose = require('mongoose')
+//
+// const attachmentSchema = new mongoose.Schema({
+//   Title: {
+//     type: String,
+//     required: true
+//   },
+//   Link: {
+//     type: String,
+//     required: true
+//   },
+//   Instructor: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Instructor'
+//   },
+//   Session: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Session'
+//   },
+//   Assignement: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Assignment'
+//   }
+// })
 
-const attachmentSchema = new mongoose.Schema({
-  Title: {
-    type: String,
-    required: true
-  },
-  Link: {
-    type: String,
-    required: true
-  },
-  Instructor: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Instructor'
-  },
-  Session: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Session'
-  },
-  Assignement: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Assignment'
-  }
-})
-
-const Attachment = mongoose.model('Attachment', attachmentSchema)
-module.exports = Attachment
+// const Attachment = mongoose.model('Attachment', attachmentSchema)
+// module.exports = Attachment
