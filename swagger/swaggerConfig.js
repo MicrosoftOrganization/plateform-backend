dd39789d092091b74da0e2563b9775ca8717abe0
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2024-12-26 20:17:46 +0100

    configuration du token dans swagger
    simple config

--- swagger/swaggerConfig.js
+++ swagger/swaggerConfig.js
@@ -1,23 +1,37 @@
-const swaggerJsdoc = require('swagger-jsdoc')
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Mon API MVC avec Express et Swagger',
-      version: '1.0.0',
+      title: "Mon API MVC avec Express et Swagger",
+      version: "1.0.0",
       description:
-        "Documentation de l'API avec Swagger dans une architecture MVC"
+        "Documentation de l'API avec Swagger dans une architecture MVC",
     },
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        BearerAuth: [],
+      },
+    ],
     servers: [
       {
-        url: 'http://localhost:4000'
-      }
-    ]
+        url: "http://localhost:4000",
+      },
+    ],
   },
-  apis: ['./routes/*.js'] // Chemin vers les fichiers de routes
-}
+  apis: ["./routes/*.js"], // Chemin vers les fichiers de routes
+};
 
-const swaggerSpec = swaggerJsdoc(options)
+const swaggerSpec = swaggerJsdoc(options);
 
-module.exports = swaggerSpec
+module.exports = swaggerSpec;
