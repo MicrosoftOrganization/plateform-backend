7b22ba9a434b38816555cb2a42cd632ee45efe98
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2024-12-26 20:37:57 +0100

    Update middleware for token

--- middlewares/VerifyToken.js
+++ middlewares/VerifyToken.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken');
-const { User } = require('../models/user');
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/user");
 
 const authenticateJWT = async (req, res, next) => {
-  const token = req.header('Authorization')?.split(' ')[1];
-  console.log(token)
-
+  const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'Access Denied: No token provided' });
+    return res
+      .status(401)
+      .json({ message: "Access Denied: No token provided" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const { id } = decoded; 
+    const { id } = decoded;
 
     const user = await User.findOne({ _id: id });
-    
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
@@ -23,16 +23,16 @@ const authenticateJWT = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    return res.status(400).json({ message: 'Invalid Token' });
+    return res.status(400).json({ message: "Invalid Token" });
   }
 };
 const authorizeRoles = (...roles) => {
   return (req, res, next) => {
-    if (!roles.includes(req.userRole)) {
-      return res.status(403).json({ message: 'Access denied' });
+    if (!roles.includes(req.user.Role)) {
+      return res.status(403).json({ message: "Access denied" });
     }
     next();
   };
 };
 
-module.exports = {authenticateJWT};
+module.exports = { authenticateJWT, authorizeRoles };
