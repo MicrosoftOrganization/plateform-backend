5147155b258c7fdebe0550dbdd53976f4dc70ec9
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2024-12-26 20:48:48 +0100

    refactoring the limit number of request

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,36 +1,36 @@
-const { User } = require('../models/user.js')
+const { User } = require("../models/user.js");
 
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const { body, validationResult } = require('express-validator')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { body, validationResult } = require("express-validator");
 
 const controller = {
   login: async (req, res) => {
     try {
-      const { email, password } = req.body
+      const { email, password } = req.body;
 
       // Recherche de l'utilisateur
-      const user = await User.findOne({ Email: email })
+      const user = await User.findOne({ Email: email });
       if (!user) {
-        return res.status(401).json({ message: 'User not found' })
+        return res.status(401).json({ message: "User not found" });
       }
 
       // Vérification du mot de passe
-      const isMatch = await bcrypt.compare(password, user.Password)
+      const isMatch = await bcrypt.compare(password, user.Password);
       if (!isMatch) {
-        return res.status(401).json({ message: 'Invalid credentials' })
+        return res.status(401).json({ message: "Invalid credentials" });
       }
 
       // Génération du token JWT
       const token = jwt.sign(
         { id: user._id, role: user.Role },
         process.env.JWT_SECRET,
-        { expiresIn: '3h' }
-      )
+        { expiresIn: "3h" }
+      );
 
       // Filtrer les informations selon le rôle
-      let filteredUser
-      if (user.Role == 'member') {
+      let filteredUser;
+      if (user.Role == "member") {
         filteredUser = {
           id: user._id,
           email: user.Email,
@@ -38,9 +38,9 @@ const controller = {
           nomPrenom: user.NomPrenom,
           adresse: user.Adresse,
           imageLink: user.ImageLink,
-          DepartmentIds: user.DepartmentIds
-        }
-      } else if (user.Role == 'instructor') {
+          DepartmentIds: user.DepartmentIds,
+        };
+      } else if (user.Role == "instructor") {
         filteredUser = {
           id: user._id,
           email: user.Email,
@@ -48,40 +48,40 @@ const controller = {
           nomPrenom: user.NomPrenom,
           adresse: user.Adresse,
           imageLink: user.ImageLink,
-          DepartmentId: user.DepartmentId
-        }
+          DepartmentId: user.DepartmentId,
+        };
       } else {
         filteredUser = {
           id: user._id,
           email: user.Email,
-          role: user.Role
-        }
+          role: user.Role,
+        };
       }
       res
-        .cookie('token', token, {
+        .cookie("Bearer", token, {
           httpOnly: true,
           secure: false,
-          sameSite: 'lax'
+          sameSite: "lax",
         })
         .status(200)
         .json({
-          message: 'Login successful',
+          message: "Login successful",
           token,
-          user: filteredUser
-        })
+          user: filteredUser,
+        });
     } catch (error) {
-      console.error('Error in authentication:', error)
-      res.status(500).json({ message: 'Error performing authentication' })
+      console.error("Error in authentication:", error);
+      res.status(500).json({ message: "Error performing authentication" });
     }
   },
   logout: (req, res) => {
-    res.clearCookie('token', {
+    res.clearCookie("token", {
       httpOnly: true,
       secure: false, // Set to false for local development
-      sameSite: 'lax' // Used SameSite=Lax for local development
-    })
-    res.status(200).json({ message: 'Logout successful' })
-  }
-}
+      sameSite: "lax", // Used SameSite=Lax for local development
+    });
+    res.status(200).json({ message: "Logout successful" });
+  },
+};
 
-module.exports = controller
+module.exports = controller;
